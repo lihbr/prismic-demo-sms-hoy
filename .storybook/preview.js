4b1905f150ae07e17214262ffdffd09765157935
lihbr <lihbr@users.noreply.github.com>
2020-06-30 18:00:26 +0200

    feat(core): add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,35 @@
+import Vue from "vue";
+import { configure } from "@storybook/vue";
+import "source-sans-pro/source-sans-variable.css"
+import "./tailwind.css";
+
+// Nuxt
+Vue.component("nuxt-link", {
+  props: ["to"],
+  methods: {
+    log() {
+      action("link target")(this.to);
+    }
+  },
+  template: "<a href=\"#\" @click.prevent=\"log()\"><slot>NuxtLink</slot></a>"
+});
+
+// Prismic
+import { common } from "@prismicio/vue/components";
+
+Object.entries(common).forEach(([_, c]) => {
+  Vue.component(c.name, c);
+});
+
+Vue.prototype.$endpoint = "https://repoz.prismic.io/api/v2";
+
+Vue.prototype.$prismic = {
+  linkResolver() {},
+  asText(richText) {
+    return richText && Array.isArray(richText) && richText[0]
+      ? richText[0].text
+      : "";
+  }
+};
+
+configure(require.context("../slices", true, /\.stories\.js$/), module);
