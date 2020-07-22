efb6b2a3d62751b478c1d570ff0ca95f297b3258
lihbr <lihbr@users.noreply.github.com>
2020-07-22 16:25:25 +0200

    feat(core): simple-spacer slice

---
+++ slices/SimpleSpacer/index.stories.js
@@ -0,0 +1,31 @@
+import { withKnobs, select } from "@storybook/addon-knobs";
+import cloneDeep from "lodash/cloneDeep";
+
+import SimpleSpacer from "./";
+import mock from "./mock.json";
+
+export default {
+  title: "Slices . Simple Spacer",
+  decorators: [withKnobs]
+};
+
+export const __Basic = () => ({
+  components: { SimpleSpacer },
+  props: {
+    mock: {
+      default: (() => {
+        const _mock = cloneDeep(mock);
+
+        _mock.primary.size = select(
+          "Space size",
+          ["Small", "Medium", "Large"],
+          _mock.primary.size
+        );
+
+        return _mock;
+      })()
+    }
+  },
+  // eslint-disable-next-line
+  template: "<SimpleSpacer :slice=\"mock\" class=\"bg-beige\" />"
+});
---
+++ slices/SimpleSpacer/index.vue
@@ -0,0 +1,22 @@
+<!-- HEALTH:UNKNOWN simple-spacer -->
+<template>
+  <hr
+    class="simpleSpacer text-transparent"
+    :class="{
+      'h-16': slice.primary.size === 'Small',
+      'h-32': slice.primary.size === 'Medium',
+      'h-64': slice.primary.size === 'Large'
+    }"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    slice: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
