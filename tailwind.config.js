d4301a638f89694ee97bef846eaa7375e647afb4
lihbr <lihbr@users.noreply.github.com>
2020-07-23 16:10:36 +0200

    fix(core): purgecss quickfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === "production",
     content: [
+      "node_modules/hoy-slices/**/*.vue",
       "components/**/*.vue",
       "layouts/**/*.vue",
       "pages/**/*.vue",
